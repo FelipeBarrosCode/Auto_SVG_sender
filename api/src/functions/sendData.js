d66fd235a4cd4cc59859f879659ed26d2b559736
FelipeBarrosCodes <0801.felipe@gmail.com>
2023-10-23 11:59:57 -0700

    Create API link

---
+++ api/src/functions/sendData.js
@@ -0,0 +1,13 @@
+te const { app } = require('@azure/functions');
+
+app.http('sendData', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const name = request.query.get('name') || await request.text() || 'world';
+
+        return { body: `Hello, ${name}!` };
+    }
+});
