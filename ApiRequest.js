db7d30d8c29523351a31fa1a7f5f4ad4e35d31f9
FelipeBarrosCode <0801.felipe@gmail.com>
2024-02-09 14:10:37 -0800

    This now works with an API good use

--- ApiRequest.js
+++ ApiRequest.js
@@ -1,32 +1,109 @@
 
-// Example POST method implementation:
-async function postData(url, data) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
-    headers: {
-      "Content-Type": "application/json",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
 
-const data = document.getElementById("form-not-click").addEventListener("submit",function(event){
-    event.preventDefault();
 
-    const name =  "" + document.getElementById("email").value
-    postData("http://localhost:3000/customer", {name: name}).then((data) => {
-    console.log(data); // JSON data parsed by `data.json()` call
-    });
+// // Example POST method implementation:
+// async function postData(url="", data={}) {
+//     // Default options are marked with *
+
+//     try {
+//         const response = await fetch(url, {
+//             method: "POST", 
+//             mode: "no-cors", 
+//             headers: {
+//                 "Content-Type": "application/json; charset=utf-8",
+//                 "Access-Control-Allow-Origin": "*",
+                
+//             },
+//             body: JSON.stringify(data) // body data type must match "Content-Type" header
+//         }).then((response) =>{
+//             return response.json()
+//         }).then((data)=>{
+//             if(data.result.ok == 1 && data.result.n==1){
+//                 let ids = buildIDS(data.document)
+//                 display.append(buildTemplate(data.document,ids))
+//             }
+//         });
+
+//         if (!response.ok) {
+//             console.log(`HTTP error! Status: ${response.statusText}`);
+//             // Handle the error here if needed
+//         } else {
+//             const result = await response.json(); // Assuming the response is JSON
+//             console.log(result);
+//         }
+//     }catch{
+
+        
+//     }
+//     finally{
+//         console.log("working")
+//     }
+
+  
     
+
+
+
+// }
+
+
+
+
+// const data = document.getElementById("form-not-click").addEventListener("submit", function (event) {
+//     event.preventDefault();
+
+    
+//     //document.getElementById("email").value
+
+//     console.log(axios.get('http://localhost:3000/customer'))
+
+// })
+
+const formin = document.getElementById('form-not-click')
+
+form.addEventListener('submit', async function(e){
+    e.preventDefault()
+
+    const payload = new FormData(formin)
+
+    
+    console.log([...payload])
+
+    const variable = payload.get("user-email");
+
+    fetch('http://localhost:3000/customer',{
+        method:"POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json"
+            
+        },
+        body: JSON.stringify({
+            "name" : variable})
+        
+        
+    }).then(response => response.json().then(json => alert(JSON.stringify(json))))
+    .then(data => console.log(data))
+    .catch(error => console.log('Error:', error.message));
+
+//     fetch('http://localhost:3000/customer')
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     return response.json();
+//   })
+//   .then(userData => {
+//     // Process the retrieved user data
+//     console.log('User Data:', userData);
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
 })
+
+
+
 
 
 
